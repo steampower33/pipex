f82cbcac979b7ec8b031ae5ccef1c55df6caaad2
SeungMinLee <seunlee2@student.42seoul.kr>
2023-06-30 18:20:29 +0900

    make pipe

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/29 17:31:22 by seunlee2          #+#    #+#              #
-#    Updated: 2023/06/29 19:40:14 by seunlee2         ###   ########.fr        #
+#    Updated: 2023/06/29 19:41:23 by seunlee2         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ SRCS = pipex.c
 OBJS = $(SRCS:.c=.o)
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
-LIBFT_PATH = libft
+LIBFT_PATH = my_libft
 
 %.o: %.c
 	$(CC) $(CFLAGS) -I $(LIBFT_PATH) -o $@ -c $<
--- pipex.c
+++ pipex.c
@@ -6,28 +6,60 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 17:06:09 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/06/29 19:24:07 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/06/30 18:19:44 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <stdio.h>
 
-// int	make_pipe(int argc char **argv)
-// {
-// 	int	fd[2];
-// 	int	pip1;
-// 	int	pip2;
+void	child1(t_pipex data)
+{
+}
 
-// 	if (pipe(fd) == -1)
-// 		exit(1);
-// }
+void	child2(t_pipex data)
+{
+}
 
-int	main(int argc, char **argv)
+int	make_pipe(t_pipex data, int argc, char **argv, char **envp)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+		exit(1);
+	pid1 = fork();
+	if (pid1 == -1)
+		exit(1);
+	else if (pid1 == 0)
+		child1(data);
+	else
+	{
+		pid2 = fork();
+		if (pid2 == -1)
+			exit(1);
+		else if (pid2 == 0)
+			child2(data);
+		else
+		{
+			close(fd[0]);
+			close(fd[1]);
+			waitpid(pid1, NULL, 0);
+			waitpid(pid2, NULL, 0);
+		}
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	data;
 
 	if (argc != 5)
 		exit(1);
 	data.infile = argv[1];
-	
+	data.cmd1 = ft_split(argv[2], ' ');
+	data.cmd2 = ft_split(argv[3], ' ');
+	data.outfile = argv[4];
+	make_pipe(data, argc, argv, envp);
 }
