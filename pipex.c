d0f8a16fa57c757608a7637b2404de61e920455d
SeungMinLee <seunlee2@student.42seoul.kr>
2023-08-07 17:43:22 +0900

    finish

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 21:51:56 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/08/01 20:35:58 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/08/07 17:42:52 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_fork(t_pipex *data, char **envp, int idx, int flag)
 {
 	pid_t	pid;
 
+	if (pipe(data->fd) == -1)
+		ft_error_handler("pipe not found", 1);
 	pid = fork();
 	if (pid == -1)
 		ft_error_handler("fork error", 1);
@@ -23,7 +25,6 @@ void	ft_fork(t_pipex *data, char **envp, int idx, int flag)
 	{
 		close(data->fd[0]);
 		dup2(data->fd[1], 1);
-		close(data->fd[1]);
 		if (flag == -1)
 			ft_error_handler("infile open error", 1);
 		if (execve(data->cmd_file[idx], data->cmd[idx], envp) == -1)
@@ -33,7 +34,6 @@ void	ft_fork(t_pipex *data, char **envp, int idx, int flag)
 	{
 		close(data->fd[1]);
 		dup2(data->fd[0], 0);
-		close(data->fd[0]);
 		waitpid(pid, NULL, WNOHANG);
 	}
 }
@@ -45,7 +45,6 @@ void	ft_exec(t_pipex *data, char **argv, char **envp)
 	data->infile_fd = open(argv[1], O_RDONLY);
 	dup2(data->infile_fd, 0);
 	close(data->infile_fd);
-	pipe(data->fd);
 	idx = 0;
 	ft_fork(data, envp, idx++, data->infile_fd);
 	while (idx < data->cmd_cnt - 1)
@@ -71,7 +70,6 @@ int	main(int argc, char **argv, char **envp)
 	if (!data->env_path)
 		ft_error_handler("envp does not exist", 1);
 	data->cmd_cnt = argc - 3;
-	data->pid_child = (int *)malloc(sizeof(int) * data->cmd_cnt);
 	if (!ft_make_cmds(data))
 		ft_error_handler("malloc error", 1);
 	ft_cmd_file(argc, data, argv);
